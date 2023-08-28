686b423c416498c444abcda4adaa7843464e7e70
Jirayu-CT <125994181+Jirayu-CT@users.noreply.github.com>
2023-08-28 22:08:41 +0700

    28/8/66

---
+++ day1/ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int distance;
+
+    distance = 700;
+
+    printf("You are %d km. away. \n", distance);
+
+    return 0;
+}
---
+++ day1/ex3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    
+    int age    = 46;
+    int height = 181;
+
+    printf("I am %d years old.\n", age);
+    printf("I am %d tall.\n", height);
+
+    return 0;
+}
